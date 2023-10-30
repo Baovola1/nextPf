f93db24d651532f07977a82528c4a6a27ac4b18f
Baovola <baovolarav@gmail.com>
2023-10-30 19:14:52 +0100

    mise en place style link UI

--- app/Components/Navigation.js
+++ app/Components/Navigation.js
@@ -1,12 +1,14 @@
 "use client";
 import React from "react";
-import Link from "next/link";
 import { useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import MenuIcon from "@mui/icons-material/Menu";
 import HomeIcon from "@mui/icons-material/Home";
 import KeyboardIcon from "@mui/icons-material/Keyboard";
 import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
+import { Box, Link } from "@mui/material";
+import { Link as RouterLink } from "next/link";
+//import Link from "next/link";
 
 function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
@@ -17,7 +19,7 @@ function Navigation() {
 
   return (
     <>
-      <header style={{ backgroundColor: "cyan" }}>
+      <header style={{ backgroundColor: "#7fffd4" }}>
         <nav className="fixed top-0 w-full flex justify-between p-4 bg-slate-200">
           <div className="sm:hidden">
             <button
@@ -30,9 +32,9 @@ function Navigation() {
               }}
             >
               {showMenu ? (
-                <CloseIcon color="secondary" />
+                <CloseIcon color="dark" />
               ) : (
-                <MenuIcon color="secondary" />
+                <MenuIcon color="dark" />
               )}
             </button>
           </div>
@@ -40,36 +42,62 @@ function Navigation() {
 
         {showMenu && (
           <div className="sm:hidden">
-            <ul
-              style={{
+            <Box
+              sx={{
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "center",
                 textAlign: "center",
               }}
             >
-              <li>
-                <Link className="block py-2 text-lg" href="/">
-                  <HomeIcon sx={{ mr: 0.8 }} fontSize="inherit" />
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="block py-2 text-lg" href="/about">
-                  <KeyboardIcon sx={{ mr: 0.8 }} fontSize="inherit" />
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link className="block py-2 text-lg" href="/contact">
-                  <ConnectWithoutContactIcon
-                    sx={{ mr: 0.8 }}
-                    fontSize="inherit"
-                  />
-                  Contact
-                </Link>
-              </li>
-            </ul>
+              <Link
+                component={RouterLink}
+                color="inherit"
+                href="/"
+                underline="hover"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <HomeIcon sx={{ mr: 0.8 }} fontSize="inherit" />
+                Home
+              </Link>
+
+              <Link
+                component={RouterLink}
+                color="inherit"
+                href="/about"
+                underline="hover"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <KeyboardIcon sx={{ mr: 0.8 }} fontSize="inherit" />
+                About
+              </Link>
+
+              <Link
+                component={RouterLink}
+                color="inherit"
+                href="/contact"
+                underline="hover"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <ConnectWithoutContactIcon
+                  sx={{ mr: 0.8 }}
+                  fontSize="inherit"
+                />
+                Contact
+              </Link>
+            </Box>
           </div>
         )}
       </header>
