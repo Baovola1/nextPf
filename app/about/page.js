bd949b182dbd80f77764b53d74fbfa9b8bf8d176
Baovola <baovolarav@gmail.com>
2023-11-06 19:06:17 +0100

    style card effet 3D

--- app/about/page.js
+++ app/about/page.js
@@ -1,41 +1,56 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Navigation from "../Components/Navigation";
 import styles from "../styles/about.module.css";
+import VanillaTilt from "vanilla-tilt";
 
 const about = () => {
+  useEffect(() => {
+    VanillaTilt.init(document.querySelectorAll(".card"), {
+      max: 25,
+      speed: 400,
+      glare: true,
+      "max-glare": 1,
+    });
+  }, []);
+
   return (
     <>
       <Navigation />
       <h1>About</h1>
       <div className={styles.parent}>
-        <div className={styles.card}>
+        <div className={`${styles.card} card`}>
           <div className={styles.content}>
-            <h2>01</h2>
-            <h3>Card One</h3>
+            <h2 className={styles.titre2}>01</h2>
+            <h3 className={styles.titre3}>Hard skills</h3>
             <p>
               « Il n'y a qu'une façon d'apprendre, c'est par l'action. » Paulo
               Coelho
             </p>
+            <button className={styles.btn}>Click here</button>
           </div>
         </div>
-        <div className={styles.card}>
+        <div className={`${styles.card} card`}>
           <div className={styles.content}>
-            <h2>02</h2>
-            <h3>Card Two</h3>
+            <h2 className={styles.titre2}>02</h2>
+            <h3 className={styles.titre3}>Soft Skills</h3>
+
             <p>
               « Il n'y a qu'une façon d'apprendre, c'est par l'action. » Paulo
               Coelho
             </p>
+            <button className={styles.btn}>Click here</button>
           </div>
         </div>
-        <div className={styles.card}>
+        <div className={`${styles.card} card`}>
           <div className={styles.content}>
-            <h2>03</h2>
-            <h3>Card Three</h3>
+            <h2 className={styles.titre2}>03</h2>
+            <h3 className={styles.titre3}>Card Three</h3>
             <p>
               « Il n'y a qu'une façon d'apprendre, c'est par l'action. » Paulo
               Coelho
             </p>
+            <button className={styles.btn}>Click here</button>
           </div>
         </div>
       </div>
