d900cdae860dc5c90dc83d4fad412ab5ec3d66b0
Baovola <baovolarav@gmail.com>
2023-11-07 17:51:06 +0100

    changement style h1 home

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Navigation from "./Components/Navigation";
 import styles from "./page.module.css";
 import { useTypewriter } from "react-simple-typewriter";
 import Framer from "./Components/Framer";
-import Cursor from "./Components/Cursor";
+//import Cursor from "./Components/Cursor";
 
 export default function Home() {
   const [typeEffect] = useTypewriter({
@@ -17,12 +17,10 @@ export default function Home() {
   return (
     <>
       <Navigation />
-
       <section className={styles.container}>
         <h1 className={styles.text1}>{typeEffect}</h1>
       </section>
       <div className={styles.main}>
-        <Cursor />
         <Framer />
       </div>
     </>
